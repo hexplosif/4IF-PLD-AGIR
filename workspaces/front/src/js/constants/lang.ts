352a5f59c88587e46eff7300ec7e08fd88ded29a
AdSchl2E <adam.schlee@insa-lyon.fr>
2025-04-07 16:19:41 +0200

    Added German and Spanish langage support for pages

--- workspaces/front/src/js/constants/lang.ts
+++ workspaces/front/src/js/constants/lang.ts
@@ -1,9 +1,11 @@
 import { Language } from "@shared/common/Languages";
 import drapeau_fr from '@app/assets/icons/drapeau_fr.webp';
 import drapeau_en from '@app/assets/icons/drapeau_en.webp';
+import drapeau_es from '@app/assets/icons/drapeau_es.webp';
+import drapeau_de from '@app/assets/icons/drapeau_de.webp';
 
 
-export const LANGUAGES: Language[] = [Language.ENGLISH, Language.FRENCH];
+export const LANGUAGES: Language[] = [Language.ENGLISH, Language.FRENCH, Language.SPANISH, Language.GERMAN];
 export const LANGUAGES_INFO = {
     [Language.ENGLISH]: {
         code: "EN",
@@ -14,5 +16,15 @@ export const LANGUAGES_INFO = {
         code: "FR",
         name: 'Français',
         img: drapeau_fr,
-    }
-}
+    },
+    [Language.SPANISH]: {
+        code: "ES",
+        name: 'Español',
+        img: drapeau_es,
+    },
+    [Language.GERMAN]: {
+        code: "DE",
+        name: 'Deutsch',
+        img: drapeau_de,
+    },
+};
