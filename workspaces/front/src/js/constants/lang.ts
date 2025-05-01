0dea3707bed5e7e673f54fe05d2eae810d6b4d38
Jixiang SUN <snownamida@outlook.com>
2025-05-01 13:42:44 +0200

    add support for portugal

--- workspaces/front/src/js/constants/lang.ts
+++ workspaces/front/src/js/constants/lang.ts
@@ -3,9 +3,10 @@ import drapeau_fr from '@app/assets/icons/drapeau_fr.webp';
 import drapeau_en from '@app/assets/icons/drapeau_en.webp';
 import drapeau_es from '@app/assets/icons/drapeau_es.webp';
 import drapeau_de from '@app/assets/icons/drapeau_de.webp';
+import drapeau_pt from '@app/assets/icons/drapeau_pt.webp';
 
 
-export const LANGUAGES: Language[] = [Language.ENGLISH, Language.FRENCH, Language.SPANISH, Language.GERMAN];
+export const LANGUAGES: Language[] = [Language.ENGLISH, Language.FRENCH, Language.SPANISH, Language.GERMAN, Language.PORTUGUESE];
 export const LANGUAGES_INFO = {
     [Language.ENGLISH]: {
         code: "EN",
@@ -26,5 +27,10 @@ export const LANGUAGES_INFO = {
         code: "DE",
         name: 'Deutsch',
         img: drapeau_de,
+    },
+    [Language.PORTUGUESE]: {
+        code: "PT",
+        name: 'Português',
+        img: drapeau_pt,
     },
 };
